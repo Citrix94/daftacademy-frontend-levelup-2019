861ea0ec18ae83eb2dd06c516fe92f699256e2c1
Adam Rogulski <kontakt@adamrogulski.com>
2019-03-31 13:02:59 +0200

    add style, and css loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import main from './script.js';
+import style from './styles/style.css';
 
 main();
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const distDir = path.resolve(__dirname, 'dist');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: distDir
     },
-
     mode: 'development',
-
+    devtool: 'eval',
+    devServer: {
+        contentBase: distDir
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
     plugins: [new HtmlWebpackPlugin({
         title: 'Daftacademy FrontEnd Level UP 2019'
     })],
-
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        }]
-    }
 };
